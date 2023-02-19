e91e26f74bb3a5b399dca69c99d21e1b6a66c9d9
Mario <mario.espasa.2003@gmail.com>
2023-02-19 17:49:03 +0100

    verify token works

--- src/middleware/authTokens.js
+++ src/middleware/authTokens.js
@@ -1,21 +1,20 @@
-const jwt = require('jsonwebtoken')
-const keys = require('../keys')
+const jwt = require("jsonwebtoken");
+const keys = require("../keys");
 
-const create = (user) =>{
-   return jwt.sign({user:user}, keys.key,{expiresIn:"2d"})
-}
+const create = (user) => {
+  return jwt.sign({ user: user }, keys.key, { expiresIn: "2d" });
+};
 
-const verify = (token)=>{
-   const payload = jwt.verify(token,keys.key)
-   if(payload != null){
-      return true
-   }else{
-      return false
-   }
-
-}
+const verify = (token) => {
+  const payload = jwt.verify(token, keys.key);
+  if (payload != null) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 module.exports = {
-   create,
-   verify,
-}
+  create,
+  verify,
+};
